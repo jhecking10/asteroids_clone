7eeb13996503d91ff4e60688803c5800bdcc9bf7
jhecking10 <jhecking10@gmail.com>
2024-11-16 22:02:25 +0900

    added delta time

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 
+clock = pygame.time.Clock()
+dt = 0
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -13,6 +16,7 @@ def main():
                 return
         pygame.Surface.fill(screen, (1, 1, 1))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
