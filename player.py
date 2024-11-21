4e4a4c93d54e19e20bf6f4c452a6809511a743e9
jhecking10 <jhecking10@gmail.com>
2024-11-21 18:01:11 +0900

    limited shooting capability to once every 0.3 seconds

--- player.py
+++ player.py
@@ -4,9 +4,11 @@ from shot import Shot
 from constants import *
 
 class Player(CircleShape):
+
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -24,11 +26,15 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * delta_time
 
     def shoot(self):
-        shot_x, shot_y = self.position.x, self.position.y
-        new_shot = Shot(shot_x, shot_y)
-        direction = pygame.Vector2(0, 1)
-        direction = direction.rotate(self.rotation)
-        new_shot.velocity = direction * PLAYER_SHOOT_SPEED
+            if self.shot_cooldown > 0:
+                return
+            else:
+                shot_x, shot_y = self.position.x, self.position.y
+                new_shot = Shot(shot_x, shot_y)
+                direction = pygame.Vector2(0, 1)
+                direction = direction.rotate(self.rotation)
+                new_shot.velocity = direction * PLAYER_SHOOT_SPEED
+                self.shot_cooldown = PLAYER_SHOOT_COOLDOWN
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -45,3 +51,4 @@ class Player(CircleShape):
             self.move(-delta_time)
         if keys[pygame.K_SPACE]:
             self.shoot()
+        self.shot_cooldown -= delta_time
